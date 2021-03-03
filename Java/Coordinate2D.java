6d83eb4f56636945c8cc9ac830f7f3d41da1cbd4
EvgeniiaJ <78631584+EvgeniiaJ@users.noreply.github.com>
2021-03-03 13:49:49 -0500

    Create Coordinate2D.java

---
+++ Java/Coordinate2D.java
@@ -0,0 +1,28 @@
+
+public class Coordinate2D {
+
+	private int xCoordinate;
+	private int yCoordinate;
+	
+	public Coordinate2D(int xCoordinate, int yCoordinate) {
+		this.xCoordinate = xCoordinate;
+		this.yCoordinate = yCoordinate;
+	}
+
+	public int getXCoordinate() {
+		return xCoordinate;
+	}
+
+	public void setXCoordinate(int xCoordinate) {
+		this.xCoordinate = xCoordinate;
+	}
+
+	public int getYCoordinate() {
+		return yCoordinate;
+	}
+
+	public void setYCoordinate(int yCoordinate) {
+		this.yCoordinate = yCoordinate;
+	}
+		
+}
